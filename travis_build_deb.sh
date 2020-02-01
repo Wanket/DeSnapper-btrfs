8411b9ba42299b08e85aedbf6d9602a56d0658e1
Wanket <ivan.alo@mail.ru>
2020-02-01 05:50:57 +0300

    Fix travis?

--- travis_build_deb.sh
+++ travis_build_deb.sh
@@ -3,10 +3,12 @@ mkdir build_deb
 (
   cd build_deb
 
-  cmake ..
+  cmake -DCMAKE_BUILD_TYPE=Release ..
   make install
 )
 
 sed -i "s/arch/$(dpkg --print-architecture)/; s/size/$(du -s deb | awk '{print $1}')/" deb/DEBIAN/control
 
 fakeroot dpkg-deb --build deb
+
+mv deb.deb desnapper-btrfs.deb
